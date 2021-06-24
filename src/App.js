3b611f992fc33841c93db8a6823abd5987c46e84
naina <nanu02d8@gmail.com>
2021-06-24 23:25:35 +0530

    Add the basic UI of the app by creating several components like header, balance etc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import Header from "./components/Header";
+import Balance from "./components/Balance";
+import TrackExpenses from "./components/TrackExpenses";
+import TransactionList from "./components/TransactionList";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <Balance />
+        <TrackExpenses />
+        <TransactionList />
+      </div>
     </div>
   );
 }
